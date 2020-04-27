a53b853c30b9cccbb5c5b859ac09b7d4a59cec2a
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2020-04-28 02:03:23 +0530

    Add files via upload

---
+++ FAC/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Pen_Paper)
+admin.site.register(Digital)
+admin.site.register(Team)
+admin.site.register(Moment)
+
